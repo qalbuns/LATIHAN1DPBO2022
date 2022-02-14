4e075c3fe91835cfc6f38f804e009cef075d541b
qalbuns <60817697+qalbuns@users.noreply.github.com>
2022-02-14 20:40:00 +0700

    Update main.java

--- sepakbolajava/main.java
+++ sepakbolajava/main.java
@@ -1,18 +1,22 @@
-class HelloWorld {
-    public static void main(String[] args) {
-    String nama_tim = "Biru";
-    System.out.println("Nama Tim : " + nama_tim);
-    
-    String asal = "Negara B";
-    System.out.println("Asal Negara : " + asal);
-    
-    String tahun = "2019";
-    System.out.println("Tahun Berdiri : " + tahun);
-    
-    System.out.println("Pemain : "); 
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args){
+    String nama_tim;
+    String asal;
+    String tahun;
     String[] pemain = {"Sadam", "Yudi", "Ade", "Ahmad"};
+    
+    nama_tim = "Biru";
+    asal = "Negara A";
+    tahun = "2019";
+   
+    System.out.println("Asal Negara : " + nama_tim);   
+    System.out.println("Asal Negara : " + asal);   
+    System.out.println("Tahun Berdiri : " + tahun);
+    System.out.println("Pemain : ");  
     for (String i : pemain){
-      System.out.println(i);
+        System.out.println(i);
     }
-    }    
+    } 
 }
