e6a12bb083945772e062495274e24dd56d8cb7f4
qalbuns <60817697+qalbuns@users.noreply.github.com>
2022-02-14 20:59:32 +0700

    Update index.php

--- sepakbolaphp/index.php
+++ sepakbolaphp/index.php
@@ -2,8 +2,10 @@
 include "sepakbola.php";
 
 	#objek
-	$sepakbola1 = new sepakbola("Biru", "Negara A", 2012, "Pemain 1");
-	$sepakbola1->display();
-
-
+	$sepakbola1 = new sepakbola("Biru", "Negara A", 2012, $pemain);
+ 	$sepakbola1->display();
+    	$pemain = array("Sadam", "Yudi", "Ade", "Ahmad");
+        foreach($pemain as $pemain){ 
+        	echo $pemain . "<br>"; 
+	}
 ?>
